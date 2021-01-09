f5994d65168514c1bd4cf5061c2bbd5778ecf84c
Al0nso <chego006@ciencias.unam.mx>
2021-01-08 23:28:36 -0600

    Withs disfrazados de funcion y aplicaciones de funcion agregadas y mejoradas

--- Practica04/parser.rkt
+++ Practica04/parser.rkt
@@ -21,21 +21,19 @@
     [(symbol? sexp) (id sexp)]
     [(number? sexp) (num sexp)]
     [(list? sexp)
-     (case (car sexp)
-       [(+) (op + (for/list ([i (cdr sexp)]) (parse i)))]
-       [(-) (op - (for/list ([i (cdr sexp)]) (parse i)))]
-       [(*) (op * (for/list ([i (cdr sexp)]) (parse i)))]
-       [(/) (op / (for/list ([i (cdr sexp)]) (parse i)))]
-       [(modulo) (op modulo (list (parse (second sexp)) (parse (third sexp))))]
-       [(exp) (op exp (list (parse (second sexp)) (parse (third sexp))))]
-       [(add1) (op add1 (list (parse (second sexp))))]
-       [(sub1) (op sub1 (list (parse (second sexp))))]
-       [(if0) (if0 (parse (second sexp)) (parse (third sexp)) (parse (fourth sexp)) ) ]
-       [(with) (with* (for/list ([i (second sexp)]) (binding (first i) (parse (second i))))
-                     (parse (third sexp)))]
-       [(with*) (with* (for/list ([i (second sexp)]) (binding (first i) (parse (second i))))
-                     (parse (third sexp)))] 
-       [(fun) ((if (repdos (second sexp))
-                   (error "parser: parámetro definido dos veces")
-                   (fun ((for/list ([i (second sexp)]) (parse i)) (parse (third sexp))) )))]
-       [(app) (app (parse (second sexp) (for/list ([i (third sexp)]) (parse i)) ))])]))
+     (cond 
+       [(equal? (car sexp) '+) (op + (for/list ([i (cdr sexp)]) (parse i)))]
+       [(equal? (car sexp) '-) (op - (for/list ([i (cdr sexp)]) (parse i)))]
+       [(equal? (car sexp) '*) (op * (for/list ([i (cdr sexp)]) (parse i)))]
+       [(equal? (car sexp) '/) (op / (for/list ([i (cdr sexp)]) (parse i)))]
+       [(equal? (car sexp) 'modulo) (op modulo (list (parse (second sexp)) (parse (third sexp))))]
+       [(equal? (car sexp) 'exp) (op exp (list (parse (second sexp)) (parse (third sexp))))]
+       [(equal? (car sexp) 'add1) (op add1 (list (parse (second sexp))))]
+       [(equal? (car sexp) 'sub1) (op sub1 (list (parse (second sexp))))]
+       [(equal? (car sexp) 'if0) (if0 (parse (second sexp)) (parse (third sexp)) (parse (fourth sexp)) ) ]
+       [(equal? (car sexp) 'with) (parse (list (list 'fun (for/list ([i (second sexp)]) (car i)) (third sexp)) (for/list ([i (second sexp)]) (second i))))] 
+       [(equal? (car sexp) 'with*) (parse (list (list 'fun (for/list ([i (second sexp)]) (car i)) (third sexp)) (for/list ([i (second sexp)]) (second i))))]  
+       [(equal? (car sexp) 'fun) (cond
+                [(not (repdos (second sexp))) (fun (second sexp) (parse (third sexp)))]
+                [ else (error "parser: parámetro definido dos veces")])]
+       [(equal? (car (first sexp)) 'fun) (app (parse (car sexp)) (for/list ([i (second sexp)]) (parse i)) )])]))
