91492c8ba529b1eb93e12ed50881bda5f4a0a653
Al0nso <chego006@hotmail.com>
2021-02-08 15:10:42 -0600

    Agrego el parse funcionando con todos los elementos del leguaje, v1.0 propio

--- Practica04/parser.rkt
+++ Practica04/parser.rkt
@@ -27,13 +27,20 @@
        [(equal? (car sexp) '*) (op * (for/list ([i (cdr sexp)]) (parse i)))]
        [(equal? (car sexp) '/) (op / (for/list ([i (cdr sexp)]) (parse i)))]
        [(equal? (car sexp) 'modulo) (op modulo (list (parse (second sexp)) (parse (third sexp))))]
-       [(equal? (car sexp) 'exp) (op exp (list (parse (second sexp)) (parse (third sexp))))]
+       [(equal? (car sexp) 'expt) (op expt (list (parse (second sexp)) (parse (third sexp))))]
        [(equal? (car sexp) 'add1) (op add1 (list (parse (second sexp))))]
        [(equal? (car sexp) 'sub1) (op sub1 (list (parse (second sexp))))]
-       [(equal? (car sexp) 'if0) (if0 (parse (second sexp)) (parse (third sexp)) (parse (fourth sexp)) ) ]
+       [(equal? (car sexp) 'if0) (if0 (parse (second sexp)) (parse (third sexp)) (parse (fourth sexp)))]
        [(equal? (car sexp) 'with) (parse (list (list 'fun (for/list ([i (second sexp)]) (car i)) (third sexp)) (for/list ([i (second sexp)]) (second i))))] 
-       [(equal? (car sexp) 'with*) (parse (list (list 'fun (for/list ([i (second sexp)]) (car i)) (third sexp)) (for/list ([i (second sexp)]) (second i))))]  
+       [(equal? (car sexp) 'with*) (parse (list (list 'fun (for/list ([i (second sexp)]) (car i)) (third sexp)) (for/list ([i (second sexp)]) (second i))))]
        [(equal? (car sexp) 'fun) (cond
                 [(not (repdos (second sexp))) (fun (second sexp) (parse (third sexp)))]
                 [ else (error "parser: parámetro definido dos veces")])]
-       [(equal? (car (first sexp)) 'fun) (app (parse (car sexp)) (for/list ([i (second sexp)]) (parse i)) )])]))
+       [(list? (first sexp))
+        (cond
+          [(equal? (car (first sexp)) 'fun) (app (parse (car sexp)) (for/list ([i (second sexp)]) (parse i)) )]
+          [else (error "parser: expresión inválida")])]
+       [(symbol? (first sexp)) (app (parse (first sexp)) (for/list ([ i (second sexp)]) (parse i)) ) ]
+       )]))
+
+
