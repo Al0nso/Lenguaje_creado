dee04b76fc83949ce4d08f7a2efe9f4bcd8e80b6
Al0nso <chego006@hotmail.com>
2021-02-08 20:18:08 -0600

    Agrego varias cosas: El parser ahora tiene las excepciones deseadas al igual que el compostamiento adecuado entre los with y with* EL interprete ya se comporta como se espera con excepción del alcance en los bindings de los with, fuera de ello tiene también las excepciones esperadas Agrego del mismo modo los archivos de pruebas y la gramática del lenguaje Parser v1.1 Interp v1.0 Test v1.0

--- Practica04/parser.rkt
+++ Practica04/parser.rkt
@@ -22,7 +22,7 @@
     [(number? sexp) (num sexp)]
     [(list? sexp)
      (cond 
-       [(equal? (car sexp) '+) (op + (for/list ([i (cdr sexp)]) (parse i)))]
+       [(equal? (car sexp) '+) (op + (for/list ([i (cdr sexp)]) (parse i))) ]
        [(equal? (car sexp) '-) (op - (for/list ([i (cdr sexp)]) (parse i)))]
        [(equal? (car sexp) '*) (op * (for/list ([i (cdr sexp)]) (parse i)))]
        [(equal? (car sexp) '/) (op / (for/list ([i (cdr sexp)]) (parse i)))]
@@ -32,13 +32,17 @@
        [(equal? (car sexp) 'sub1) (op sub1 (list (parse (second sexp))))]
        [(equal? (car sexp) 'if0) (if0 (parse (second sexp)) (parse (third sexp)) (parse (fourth sexp)))]
        [(equal? (car sexp) 'with) (parse (list (list 'fun (for/list ([i (second sexp)]) (car i)) (third sexp)) (for/list ([i (second sexp)]) (second i))))] 
-       [(equal? (car sexp) 'with*) (parse (list (list 'fun (for/list ([i (second sexp)]) (car i)) (third sexp)) (for/list ([i (second sexp)]) (second i))))]
+       [(equal? (car sexp) 'with*) (cond
+                                     [(equal? (length (second sexp)) 1) (parse (list 'with (second sexp) (third sexp))) ]
+                                     [else (parse (list 'with (list (car (second sexp))) (list 'with* (cdr (second sexp))  (third sexp)) )) ])]
        [(equal? (car sexp) 'fun) (cond
                 [(not (repdos (second sexp))) (fun (second sexp) (parse (third sexp)))]
                 [ else (error "parser: parámetro definido dos veces")])]
        [(list? (first sexp))
         (cond
-          [(equal? (car (first sexp)) 'fun) (app (parse (car sexp)) (for/list ([i (second sexp)]) (parse i)) )]
+          [(equal? (car (first sexp)) 'fun) (cond
+                                              [(equal? (length (second (car sexp))) (length (second sexp))) (app (parse (car sexp)) (for/list ([i (second sexp)]) (parse i)) )]
+                                              [else (error "parser: La cardinalidad de los argumentos difiere de la aridad de la función" (second sexp))])]
           [else (error "parser: expresión inválida")])]
        [(symbol? (first sexp)) (app (parse (first sexp)) (for/list ([ i (second sexp)]) (parse i)) ) ]
        )]))
